631a16509f7aa84018506d06dd60f351e2aa118b
Aul1603 <asadakamol@gmail.com>
2023-11-03 12:59:31 +0530

    Initial commit

---
+++ Lab_Program/src/First.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+	    int  x= Integer.parseInt(args[0]);
+		int y=Integer.parseInt(args[1]);
+		int c=x+y;
+		System.out.println(c);
+	}
+}
